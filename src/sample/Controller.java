14993d57b3caad144ff9375448a342d996315962
q <zkacper03@gmail.com>
2019-04-22 21:22:59 +0200

    almost finito

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,20 +3,21 @@ package sample;
 // nie działa 5*= = = = =
 // nie dziala klawiatura
 // nie przedzialek w =
+// dodac flage czy ostatnie byla cyfra czy znak
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
-import static java.lang.Math.round;
-import static java.lang.Math.sqrt;
+import static java.lang.Math.*;
 
 
 public class Controller {
     private double memory = 0;
     private String operator = "";
     private boolean begin = true;
+    private boolean error = false;
 
     CalculateInterface add = (double x, double y) -> x + y;
     CalculateInterface subtract = (double x, double y) -> x - y;
@@ -42,18 +43,15 @@ public class Controller {
                 value = divide.Calculate(x, y);
                 break;
             case "√":
+                if (x < 0) throw new ArithmeticException();
                 value = squareRoot.Calculate(x, 0);
-                result.setText(Double.toString(value));
-                operator = "";
-                memory = 0;
-                begin = true;
                 break;
             default:
                 value = 0;
         }
         value = round(value * scale) / scale;
         int remainder = (int) value;
-        if (value - remainder < 5 / scale) return remainder;
+        if (abs(value - remainder) < 5 / scale) return remainder;
         return value;
     }
 
@@ -61,12 +59,19 @@ public class Controller {
     public Label result;
 
     @FXML
+    public void changeSign() {
+        if (Double.parseDouble(result.getText()) > 0)
+            result.setText("-" + result.getText());
+        else if (Double.parseDouble(result.getText()) < 0) result.setText(result.getText().substring(1));
+    }
+
+    @FXML
     public void processNum(ActionEvent event) {
         if (begin)
             if (!((Button) event.getSource()).getText().equals("0")) {
                 result.setText("");
                 begin = false;
-            }
+            } else result.setText("0");
 
         if (!begin) {
             String value = "";
@@ -81,29 +86,34 @@ public class Controller {
 
     @FXML
     public void processOperator(ActionEvent event) {
-        if (!((Button) event.getSource()).getText().equals("=")) {
-            if (!operator.isEmpty()) {
-                if (!begin) {
-                    try {
-                        memory = Calc(memory, Double.parseDouble(result.getText()), operator);
-                        result.setText(Double.toString(memory));
-                    } catch (ArithmeticException a) {
-                        operator = "";
-                        memory = 0;
-                        result.setText("You can't divide by zero!");
-                    }
+        if (!operator.isEmpty()) {
+            if (!begin) {
+                try {
+                    memory = Calc(memory, Double.parseDouble(result.getText()), operator);
+                    result.setText(Double.toString(memory));
+                } catch (ArithmeticException a) {
+                    error = true;
+                    operator = "";
+                    memory = 0;
+                    result.setText("You cannot perform this operation!");
                 }
-            } else memory = Double.parseDouble(result.getText());
-            operator = ((Button) event.getSource()).getText();
-        } else {
-            try {
-                result.setText(Double.toString(Calc(memory, Double.parseDouble(result.getText()), operator)));
-            } catch (ArithmeticException a) {
-                result.setText("You can't divide by zero!");
-            } finally {
-                operator = "";
-                memory = 0;
             }
+        } else if (error) return;
+        else memory = Double.parseDouble(result.getText());
+        operator = ((Button) event.getSource()).getText();
+        begin = true;
+    }
+
+    @FXML
+    void equalsSign() {
+        try {
+            result.setText(Double.toString(Calc(memory, Double.parseDouble(result.getText()), operator)));
+        } catch (ArithmeticException a) {
+            error = true;
+            result.setText("You cannot perform this operation!");
+        } finally {
+            operator = "";
+            memory = 0;
         }
         begin = true;
     }
@@ -114,10 +124,12 @@ public class Controller {
         memory = 0;
         result.setText("0");
         begin = true;
+        error = false;
     }
 
     @FXML
     public void delete() {
+        if (error) return;
         if (!result.getText().equals("0")) {
             if (result.getText().length() > 1) {
                 result.setText(result.getText().substring(0, result.getText().length() - 1));
@@ -126,11 +138,14 @@ public class Controller {
                         result.setText(result.getText().substring(0, result.getText().length() - 1));
                     if ((result.getText().charAt(result.getText().length() - 1) == '.'))
                         result.setText(result.getText().substring(0, result.getText().length() - 1));
+                } else if
+                (result.getText().length() == 1 && result.getText().contains("-")) {
+                    result.setText("0");
                 }
             } else {
-                begin = true;
                 result.setText("0");
             }
-        } else begin = true;
+        }
+        if (result.getText().equals("0")) begin = true;
     }
 }
